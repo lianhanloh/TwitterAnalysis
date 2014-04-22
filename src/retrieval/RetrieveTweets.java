42c4a7e95cde525aa1a1df258e8e1ba094cb9210
Nathaniel Chan <channat@seas.upenn.edu>
2014-04-21 22:35:55 -0400

    More efficient retrieval of followings list

--- src/retrieval/RetrieveTweets.java
+++ src/retrieval/RetrieveTweets.java
@@ -6,11 +6,13 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 
 import twitter4j.IDs;
+import twitter4j.PagableResponseList;
 import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 
 /**
  * 
@@ -21,30 +23,38 @@ import twitter4j.TwitterFactory;
  */
 public class RetrieveTweets {
 	
-	private static HashMap<Long, String> followingUsers;
+//	private static HashMap<Long, String> followingUsers;
+	private static PagableResponseList<User> users;
 
 	public static void main(String[] args) {
-		followingUsers = new HashMap<Long, String>();
+//		followingUsers = new HashMap<Long, String>();
 		try {
 			Twitter twitter = new TwitterFactory().getInstance();
-			IDs ids = 
-					twitter.getFriendsIDs("natc221", (long) -1);
-			for (long x: ids.getIDs()) {
-				followingUsers.put(x, twitter.showUser(x).getName());
-//				System.out.println(twitter.showUser(x).getName());
-			}
+			users = twitter.getFriendsList("natc221", (long) -1);
+//			for (long x: ids.getIDs()) {
+//				followingUsers.put(x, twitter.showUser(x).getName());
+////				System.out.println(twitter.showUser(x).getName());
+//			}
 		} catch (TwitterException te) {
-//			te.printStackTrace();
+			te.printStackTrace();
 			System.out.println("Failed to delete status: " + te.getMessage());
 //			System.exit(-1);
 		}
 		System.out.println("Done parsing following");
-		Collection<Entry<Long, String>> names = followingUsers.entrySet();
-		Iterator<Entry<Long, String>> it = names.iterator();
+		Iterator it = users.iterator();
 		while (it.hasNext()) {
-			Entry<Long, String> entry = (Entry<Long, String>) it.next();
-			System.out.println(entry.getKey() + ": " + entry.getValue());
+			User user = (User) it.next();
+			System.out.println(user.getName());
 		}
+		
+		
+		
+//		Collection<Entry<Long, String>> names = followingUsers.entrySet();
+//		Iterator<Entry<Long, String>> it = names.iterator();
+//		while (it.hasNext()) {
+//			Entry<Long, String> entry = (Entry<Long, String>) it.next();
+//			System.out.println(entry.getKey() + ": " + entry.getValue());
+//		}
 	}
 
 	
