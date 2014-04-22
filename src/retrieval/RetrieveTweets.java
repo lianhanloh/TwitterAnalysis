aab6d53872738b828e126b468d9abbd00c6ec6b7
Nathaniel Chan <channat@seas.upenn.edu>
2014-04-21 20:26:02 -0400

    Temporary changes

--- src/retrieval/RetrieveTweets.java
+++ src/retrieval/RetrieveTweets.java
@@ -7,12 +7,8 @@ package retrieval;
 public class RetrieveTweets {
 
 	public static void main(String[] args) {
-
-		System.out.println("This is a branched version");
-		check();
+		
+		
 	}
 
-	private static void check() {
-
-	}
 }
