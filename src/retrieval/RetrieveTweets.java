6758a52e9db9cd1e1a83a7ec6871eaf4c8b0fd49
Nathaniel Chan <channat@seas.upenn.edu>
2014-04-21 22:26:37 -0400

    Printing IDs and names of following users

--- src/retrieval/RetrieveTweets.java
+++ src/retrieval/RetrieveTweets.java
@@ -1,6 +1,9 @@
 package retrieval;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
 
 import twitter4j.IDs;
 import twitter4j.ResponseList;
@@ -17,20 +20,36 @@ import twitter4j.TwitterFactory;
  *
  */
 public class RetrieveTweets {
+	
+	private static HashMap<Long, String> followingUsers;
 
 	public static void main(String[] args) {
+		followingUsers = new HashMap<Long, String>();
 		try {
 			Twitter twitter = new TwitterFactory().getInstance();
 			IDs ids = 
 					twitter.getFriendsIDs("natc221", (long) -1);
 			for (long x: ids.getIDs()) {
-				System.out.println(twitter.showUser(x).getName());
+				followingUsers.put(x, twitter.showUser(x).getName());
+//				System.out.println(twitter.showUser(x).getName());
 			}
 		} catch (TwitterException te) {
-			te.printStackTrace();
+//			te.printStackTrace();
 			System.out.println("Failed to delete status: " + te.getMessage());
-			System.exit(-1);
+//			System.exit(-1);
+		}
+		System.out.println("Done parsing following");
+		Collection<Entry<Long, String>> names = followingUsers.entrySet();
+		Iterator<Entry<Long, String>> it = names.iterator();
+		while (it.hasNext()) {
+			Entry<Long, String> entry = (Entry<Long, String>) it.next();
+			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
 	}
 
+	
+	
+	
+	
+	
 }
