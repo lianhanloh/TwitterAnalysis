21d42db9a8a8480dfaf925c54594ab94b36f0207
lianhanloh <loh.lianhan@gmail.com>
2014-04-24 12:59:08 -0400

    committing to pull

--- pullFollowersData.sh
+++ pullFollowersData.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# this script runs the .jar to retrieve the next set of followers
+# this script runs the .jar to retrieve the next set of followers and following
 
 cd ~/Documents/nets150/TweetAnalysis
-java -jar tweetAnalysis_v1.jar 2>> errors.txt
-cur=$(cat count.txt | tail -1)
-echo $((cur + 1)) >> count.txt
-sleep 30
 java -jar tweetAnalysis_v1.jar 2>> errors.txt
 cur=$(cat count.txt | tail -1)
 echo $((cur + 1)) >> count.txt
