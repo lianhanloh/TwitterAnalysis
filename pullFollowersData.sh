19d3899404f757a6aead89cebcbaee03cdcb2928
Lianhan <lianhan@seas.upenn.edu>
2014-04-23 23:44:41 -0400

    errors.txt gets all the error messages from running the program

--- pullFollowersData.sh
+++ pullFollowersData.sh
@@ -3,10 +3,10 @@
 # this script runs the .jar to retrieve the next set of followers
 
 cd ~/Documents/nets150/TweetAnalysis
-java -jar tweetAnalysis_v1.jar
+java -jar tweetAnalysis_v1.jar 2>> errors.txt
 cur=$(cat count.txt | tail -1)
 echo $((cur + 1)) >> count.txt
 sleep 30
-java -jar tweetAnalysis_v1.jar
+java -jar tweetAnalysis_v1.jar 2>> errors.txt
 cur=$(cat count.txt | tail -1)
 echo $((cur + 1)) >> count.txt
