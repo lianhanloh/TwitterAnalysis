c5f6c8a7bd986ad9936a5f51176a09a4d05c7bfb
Nathaniel Chan <channat@seas.upenn.edu>
2014-04-24 17:47:52 -0400

    implement retrieve tweets and sortUser

--- src/retrieval/RetrieveTweets.java
+++ src/retrieval/RetrieveTweets.java
@@ -1,99 +1,70 @@
 package retrieval;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Writer;
-import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
-
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import twitter4j.IDs;
 import twitter4j.JSONArray;
-import twitter4j.JSONException;
-import twitter4j.JSONObject;
-import twitter4j.PagableResponseList;
 import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.User;
 
 /**
  * 
- * This program retrieves tweets from Twitter using the twitter4j API
+ * This program retrieves tweets from Twitter using the twitter4j API,
+ * then stores it in a JSON file
  * 
  * @author natchan and lianhanloh
  *
  */
 public class RetrieveTweets {
 
-	//	private static ArrayList<User> allUsers;
-	//	private static PagableResponseList<User> users;
-	//	private static String outputFile = "output2.txt";
-	//	private static Writer out;
 	private static final String NODE_LIST = "nodeList.txt";
 	private static final String JSON_FILE = "userTweets.json";
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		
-		JSONParser parser = new JSONParser();
-		JSONObject json = new JSONObject();
-		try {
-			JSONArray tweets = new JSONArray();
-			tweets.put("hello world!");
-			tweets.put("boooo");
-			json.put("123", tweets);
-			
-			tweets.put("foo");
-			tweets.put("bar");
-			json.put("321", tweets);
-			FileWriter out = new FileWriter(JSON_FILE);
-			out.write(json.toString());
-			out.flush();
-			out.close();
-			
-			
-			
-		} catch (JSONException e1) {
-			e1.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(json);
-		
-		/*
 		BufferedReader in = null;
-		
-
+		JSONParser parser = new JSONParser();
 		try {
 			in = new BufferedReader(new FileReader(NODE_LIST));
-
-			String line = null;
-			while ((line = in.readLine()) != null) {
-				long userID = Long.parseLong(line.trim());
+			String user = in.readLine();
+			if (user != null) {
+				long userID = Long.parseLong(user);
 				Twitter twitter = new TwitterFactory().getInstance();
 				ResponseList<Status> statuses = 
 						twitter.getUserTimeline(userID);
 				
+				JSONArray tweets = new JSONArray();
+				for (Status s: statuses) {
+					tweets.put(s);
+				}
+				
+				Object obj = parser.parse(new FileReader(JSON_FILE));
+				JSONObject json = (JSONObject) obj;
 
-
+				json.put(user, tweets);
+				FileWriter out = new FileWriter(JSON_FILE);
+				out.write(json.toString());
+				out.flush();
+				out.close();
+				
+				System.out.println(json);
 			}
+
 
 
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
 		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		finally {
@@ -103,62 +74,7 @@ public class RetrieveTweets {
 				e.printStackTrace();
 			}
 		}
-		*/
-
-
-		/*
-		try {
-			out = new BufferedWriter(new FileWriter(outputFile));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		int requests = 0;
-		try {
-			Twitter twitter = new TwitterFactory().getInstance();
-			allUsers = new ArrayList<User>();
-
-			//while there is a next page, get users
-			long cursor = -1; //start with page one, cursor as -1
-			do {
-				users = twitter.getFriendsList(17461978, cursor);
-				requests++;
-				allUsers.addAll(users);
-				cursor = users.getNextCursor();
-			}
-			while (users.hasNext());
-
-		} catch (TwitterException te) {
-			te.printStackTrace();
-			System.out.println("Failed to delete status: " + te.getMessage());
-			//			System.exit(-1);
-		}
-		finally {
-			System.out.println("Requests made: " + requests);
-		}
-		System.out.println("Done parsing following");
-		Iterator<User> it = allUsers.iterator();
-		while (it.hasNext()) {
-			User user = (User) it.next();
-			System.out.println(user.getName());
-			System.out.println(user.getId());
-			System.out.println(user.getURL());
-			System.out.println();
-
-			try {
-				out.write(user.getName() + ", " + user.getURL() + '\n');
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		 */
 
 	}
-
-
-
-
-
 
 }
--- src/retrieval/SortUsers.java
+++ src/retrieval/SortUsers.java
@@ -6,26 +6,33 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 
+/**
+ * Finds the unique users from edge list, then sorts the user IDs
+ * and prints them to an external text file
+ * 
+ * @author NatChan and lianhanloh
+ *
+ */
 public class SortUsers {
-	
+
 	private static final String EDGE_LIST = "edgeList.txt";
 	private static final String NODE_LIST = "nodeList.txt";
 
 	public static void main(String[] args) {
-		ArrayList<Long> userSet = new ArrayList<Long>();
-		
+		System.out.println("Sorting nodes from " + EDGE_LIST);
+		HashSet<Long> userSet = new HashSet<Long>();
+
 		BufferedReader in = null;
 		BufferedWriter out = null;
-		
+
 		try {
 			in = new BufferedReader(new FileReader(EDGE_LIST));
 			out = new BufferedWriter(new FileWriter(NODE_LIST));
-			
+
 			String line;
 			while ((line = in.readLine()) != null) {
 				String[] split = line.split(" ");
@@ -36,24 +43,35 @@ public class SortUsers {
 				}
 				long from = Long.parseLong(split[0]);
 				long to = Long.parseLong(split[1]);
-				
+
+				//add users to set
 				userSet.add(from);
 				userSet.add(to);
 			}
-			
-			Collections.sort(userSet);
-			
-			for (long x: userSet) {
+
+			//sort the list of users
+			System.out.println("Sorting...");
+			long startTime = System.currentTimeMillis();
+			List<Long> list = new ArrayList<Long>(userSet);
+			Collections.sort(list);
+			long endTime = System.currentTimeMillis();
+			long duration = endTime - startTime;
+			System.out.println("User sort complete");
+			System.out.println("Sort took " + Long.toString(duration) + "ms");
+
+			//print user list to text file
+			for (long x: list) {
 				out.write(Long.toString(x));
 				out.newLine();
-
 			}
-			
-			
+			System.out.println("Nodes written to " + NODE_LIST);
+
+
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		finally {
 			try {
 				out.close();
@@ -62,7 +80,7 @@ public class SortUsers {
 				e.printStackTrace();
 			}
 		}
-		
+
 
 	}
 
