c644413297bf77b54385b913e3d95b0d874576a3
Nathaniel Chan <channat@seas.upenn.edu>
2014-04-24 18:36:25 -0400

    More StanfordNLP libraries and testing

--- src/com/sentiments/analyzers/SentimentAnalyzer.java
+++ src/com/sentiments/analyzers/SentimentAnalyzer.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.rnn.RNNCoreAnnotations;
+//import edu.stanford.nlp.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
@@ -23,7 +23,7 @@ public class SentimentAnalyzer {
             Annotation annotation = pipeline.process(line);
             for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
                 Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+                int sentiment = edu.stanford.nlp.neural.rnn.RNNCoreAnnotations.getPredictedClass(tree);
                 String partText = sentence.toString();
                 if (partText.length() > longest) {
                     mainSentiment = sentiment;
