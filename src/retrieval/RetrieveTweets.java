3b991d55a9688286e05ba94f8c928f3e2dd7e89f
Nathaniel Chan <channat@seas.upenn.edu>
2014-04-24 18:19:16 -0400

    Added sentiment analysis source code + StanfordNLP
    Source code from:
    https://www.openshift.com/blogs/day-20-stanford-corenlp-performing-senti
    ment-analysis-of-twitter-using-java
    
    Stanford NLP:
    http://www-nlp.stanford.edu/software/corenlp.shtml

--- src/retrieval/RetrieveTweets.java
+++ src/retrieval/RetrieveTweets.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import twitter4j.HashtagEntity;
 import twitter4j.JSONArray;
 import twitter4j.ResponseList;
 import twitter4j.Status;
@@ -25,6 +27,8 @@ import twitter4j.TwitterFactory;
  */
 public class RetrieveTweets {
 
+	//node list acts as a queue, of which nodes whose tweets need to be
+	//pulled
 	private static final String NODE_LIST = "nodeList.txt";
 	private static final String JSON_FILE = "userTweets.json";
 
@@ -35,26 +39,47 @@ public class RetrieveTweets {
 		try {
 			in = new BufferedReader(new FileReader(NODE_LIST));
 			String user = in.readLine();
-			if (user != null) {
+
+			if (user != null && !user.equals("")) {
 				long userID = Long.parseLong(user);
 				Twitter twitter = new TwitterFactory().getInstance();
 				ResponseList<Status> statuses = 
 						twitter.getUserTimeline(userID);
-				
+
 				JSONArray tweets = new JSONArray();
+				JSONArray hashtags = new JSONArray();
+
+				//get all tweets and hashtags
 				for (Status s: statuses) {
-					tweets.put(s);
+					tweets.put(s.getText());
+
+					//get all the hashtags associated
+					for (HashtagEntity tag: s.getHashtagEntities()) {
+						hashtags.put(tag.getText());
+					}
+				}
+
+				FileReader file = new FileReader(JSON_FILE);
+				JSONObject json = null;
+				//if JSON file is empty
+				if (file.read() == -1) {
+					json = new JSONObject();
+				}
+				//read from existing JSON file
+				else {
+					Object obj = parser.parse(file);
+					json = (JSONObject) obj;
 				}
 				
-				Object obj = parser.parse(new FileReader(JSON_FILE));
-				JSONObject json = (JSONObject) obj;
-
+				//put tweets and hashtags into json
 				json.put(user, tweets);
+				json.put(user, hashtags);
+
 				FileWriter out = new FileWriter(JSON_FILE);
 				out.write(json.toString());
 				out.flush();
 				out.close();
-				
+
 				System.out.println(json);
 			}
 
