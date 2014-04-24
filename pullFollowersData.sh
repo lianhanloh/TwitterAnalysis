f8a9e1e1192ac7e0d561474204e5c7a10eb6f427
Lianhan <lianhan@seas.upenn.edu>
2014-04-23 23:34:55 -0400

    wrote shell script to run .jar file meant to pull twitter data, count.txt indicates number of times .jar file has been run. crontab set up on eniac to run every minute.

---
+++ pullFollowersData.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this script runs the .jar to retrieve the next set of followers
+
+cd ~/Documents/nets150/TweetAnalysis
+java -jar tweetAnalysis_v1.jar
+cur=$(cat count.txt | tail -1)
+echo $((cur + 1)) >> count.txt
+sleep 30
+java -jar tweetAnalysis_v1.jar
+cur=$(cat count.txt | tail -1)
+echo $((cur + 1)) >> count.txt
