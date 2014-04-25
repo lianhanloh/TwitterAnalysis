eeeac2f98ac1a99cdf84aeb8083b8d40885a7342
Nathaniel Chan <channat@seas.upenn.edu>
2014-04-25 15:34:07 -0400

    modified script path

--- pullFollowersData.sh
+++ pullFollowersData.sh
@@ -2,7 +2,7 @@
 
 # this script runs the .jar to retrieve the next set of followers and following
 
-cd ~/Documents/nets150/TweetAnalysis
+cd ~/Desktop/TweetAnalysis
 java -jar retrieveFriends.jar 2>> errors.txt
 cur=$(cat count.txt | tail -1)
 echo $((cur + 1)) > count.txt
