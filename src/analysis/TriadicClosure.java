e4c0b374bd7c9ae06730a45b3b354f66d770a89d
Lianhan Loh <loh.lianhan@gmail.com>
2014-05-01 16:05:18 -0400

    added graph package to populate graph of users

--- src/analysis/TriadicClosure.java
+++ src/analysis/TriadicClosure.java
@@ -1,53 +1,7 @@
 package analysis;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Iterator;
-
-import org.apache.commons.io.IOUtils;
-
-import twitter4j.JSONException;
-import twitter4j.JSONObject;
-
 
 public class TriadicClosure {
 
-    /** class fields */
-    private static final String JSON_FILE = "adjacencyList.json";
-
-
-    public static void main(String[] args) {
-
-        // open and read json file
-        try {
-            InputStream is = new FileInputStream(JSON_FILE);
-            String jsonTxt = IOUtils.toString(is);
-            JSONObject json = new JSONObject(jsonTxt);
-            Iterator it = json.keys();
-            
-            int i = 0;
-            while (it.hasNext()) {
-                String id = (String) it.next();
-                System.out.println("Twitter user " + i++ + " : " + id);
-            }
-       
-            is.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Internal error: " 
-                    + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("Internal error: " 
-                    + e.getMessage());
-            e.printStackTrace();
-        } catch (JSONException e) {
-            System.out.println("Internal error: " 
-                    + e.getMessage());
-            e.printStackTrace();
-        }
-
-    }
-
+   
 }
