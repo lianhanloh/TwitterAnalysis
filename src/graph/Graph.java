c77fcfab7bcf14cd55efc935f63ed5890f8826fc
Lianhan Loh <loh.lianhan@gmail.com>
2014-05-02 01:33:33 -0400

    refactored graph.java

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -35,44 +35,11 @@ public class Graph {
         try {
             InputStream is = new FileInputStream(JSON_FILE);
             String jsonTxt = IOUtils.toString(is);
-            JSONObject json = new JSONObject(jsonTxt);
-            @SuppressWarnings("rawtypes")
-            Iterator it = json.keys();
-
-            while (it.hasNext()) {
-                String id_string = (String) it.next();
-                long id = Long.parseLong(id_string);
-                User user = new User(id);
-                HashSet<User> following = new HashSet<User>();
-                HashSet<User> followers = new HashSet<User>();
-                // get user's json object
-                JSONObject userJSON = json.getJSONObject(id_string);
-                JSONArray followersJSON = userJSON.getJSONArray("followers");
-                // get followers and add to hash set
-                int numFollowers = followersJSON.length();
-                for (int j = 0; j < numFollowers; j++) {
-                    User follower = new User(followersJSON.getLong(j));
-                    if (!followers.add(follower) ) {
-                        throw new RuntimeException("Repeat followers at user "
-                                + id + ": " + "follower number: " + j);
-                    }
-                }
-                user.setFollowers(followers);
-                // get following and set to hash set
-                JSONArray followingJSON = userJSON.getJSONArray("following");
-                int numFollowing = followingJSON.length();
-                for (int j = 0; j < numFollowing; j++) {
-                    User friend = new User(followingJSON.getLong(j));
-                    if (!following.add(friend) ) {
-                        throw new RuntimeException("Repeat friend at user "
-                                + id + ": " + "friend number: " + j);
-                    }
-                }
-                user.setFollowing(following);
-                // add to set of users
-                allUsers.add(user);
-            }
-
+            populateGraph(jsonTxt);
+            is.close();
+            is = new FileInputStream(JSON_FILE_LH);
+            jsonTxt = IOUtils.toString(is);
+            populateGraph(jsonTxt);
             is.close();
         } catch (FileNotFoundException e) {
             System.out.println("Internal error: " 
@@ -87,66 +54,51 @@ public class Graph {
                     + e.getMessage());
             e.printStackTrace();
         }
-        try{
-            InputStream is = new FileInputStream(JSON_FILE_LH);
-            String jsonTxt = IOUtils.toString(is);
-            JSONObject json = new JSONObject(jsonTxt);
-            @SuppressWarnings("rawtypes")
-            Iterator it = json.keys();
+    }
 
-            while (it.hasNext()) {
-                String id_string = (String) it.next();
-                long id = Long.parseLong(id_string);
-                User user = new User(id);
-                // skip if user has already been added to all users set
-                if (allUsers.contains(user)) {
-                    continue;
+    /**
+     * given a json text string, parses the information and populates graph
+     * @param jsonTxt
+     * @throws JSONException
+     */
+    private void populateGraph(String jsonTxt) throws JSONException {
+        JSONObject json = new JSONObject(jsonTxt);
+        @SuppressWarnings("rawtypes")
+        Iterator it = json.keys();
+
+        while (it.hasNext()) {
+            String id_string = (String) it.next();
+            long id = Long.parseLong(id_string);
+            User user = new User(id);
+            HashSet<User> following = new HashSet<User>();
+            HashSet<User> followers = new HashSet<User>();
+            // get user's json object
+            JSONObject userJSON = json.getJSONObject(id_string);
+            JSONArray followersJSON = userJSON.getJSONArray("followers");
+            // get followers and add to hash set
+            int numFollowers = followersJSON.length();
+            for (int j = 0; j < numFollowers; j++) {
+                User follower = new User(followersJSON.getLong(j));
+                if (!followers.add(follower) ) {
+                    throw new RuntimeException("Repeat followers at user "
+                            + id + ": " + "follower number: " + j);
                 }
-                HashSet<User> following = new HashSet<User>();
-                HashSet<User> followers = new HashSet<User>();
-                // get user's json object
-                JSONObject userJSON = json.getJSONObject(id_string);
-                JSONArray followersJSON = userJSON.getJSONArray("followers");
-                // get followers and add to hash set
-                int numFollowers = followersJSON.length();
-                for (int j = 0; j < numFollowers; j++) {
-                    User follower = new User(followersJSON.getLong(j));
-                    if (!followers.add(follower) ) {
-                        throw new RuntimeException("Repeat followers at user "
-                                + id + ": " + "follower number: " + j);
-                    }
-                }
-                user.setFollowers(followers);
-                // get following and set to hash set
-                JSONArray followingJSON = userJSON.getJSONArray("following");
-                int numFollowing = followingJSON.length();
-                for (int j = 0; j < numFollowing; j++) {
-                    User friend = new User(followingJSON.getLong(j));
-                    if (!following.add(friend) ) {
-                        throw new RuntimeException("Repeat friend at user "
-                                + id + ": " + "friend number: " + j);
-                    }
-                }
-                user.setFollowing(following);
-                // add to set of users
-                allUsers.add(user);
             }
-
-            is.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Internal error: " 
-                    + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("Internal error: " 
-                    + e.getMessage());
-            e.printStackTrace();
-        } catch (JSONException e) {
-            System.out.println("Internal error: " 
-                    + e.getMessage());
-            e.printStackTrace();
+            user.setFollowers(followers);
+            // get following and set to hash set
+            JSONArray followingJSON = userJSON.getJSONArray("following");
+            int numFollowing = followingJSON.length();
+            for (int j = 0; j < numFollowing; j++) {
+                User friend = new User(followingJSON.getLong(j));
+                if (!following.add(friend) ) {
+                    throw new RuntimeException("Repeat friend at user "
+                            + id + ": " + "friend number: " + j);
+                }
+            }
+            user.setFollowing(following);
+            // add to set of users
+            allUsers.add(user);
         }
-
     }
 
     /** returns all the users */
