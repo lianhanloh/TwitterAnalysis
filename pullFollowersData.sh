1275dc08b2e13205e88a0e304e763ff49cfc4d5c
Lianhan Loh <loh.lianhan@gmail.com>
2014-05-04 19:08:21 -0400

    deleted extra files

--- pullFollowersData.sh
+++ pullFollowersData.sh
@@ -2,7 +2,8 @@
 
 # this script runs the .jar to retrieve the next set of followers and following
 
-cd ~/Desktop/TweetAnalysis
+# update the file path to where the .jar is located before running the script
+cd ~/Path/to/your/Project/
 java -jar retrieveFriends.jar 2>> errors.txt
 cur=$(cat count.txt | tail -1)
 echo $((cur + 1)) > count.txt
--- pullFollowersData_lh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this script runs the retrieveFriends_lh.jar to retrieve the next set of followers and following
-# this will run parallel to the existing run
-
-cd ~/Documents/nets150/TweetAnalysis
-java -jar retrieveFriends_lh.jar 2>> errors_lh.txt
-cur=$(cat count_lh.txt | tail -1)
-echo $((cur + 1)) > count_lh.txt
